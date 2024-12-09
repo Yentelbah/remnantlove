e11997d3b2cd218a11522437f4e05e89c350d913
Yentelbahm <yentelbah@gmail.com>
2024-12-09 03:51:06 +0000

    first

--- deploy.php
+++ deploy.php
@@ -8,8 +8,6 @@ set('ssh_type', 'native');
 set('ssh_multiplexing', false);
 set('git_tty', false);
 
-set('repository', 'https://Yentelbah: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@github.com/Yentelbah/remnantlove.git');
-
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
